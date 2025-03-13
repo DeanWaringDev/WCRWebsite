0b5bd83596d24ce3e0197e2b61229ce69583a215
deanw77 <deanwaring77@outlook.com>
2025-03-13 10:43:07 +0000

    Basic Header Setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,68 @@
 import React from "react";
+import { useState } from "react";
+
+import Logo from "../assets/images/LogoSmall.png";
+
+import "../css/header.css";
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+  const toggleClass = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <>
-      <h1>Hello World</h1>
+      <header>
+        <div className="container">
+          <div className="flexOne">
+            <div>
+              <img src={Logo} alt="" />
+            </div>
+
+            <h1>Wheelchair Racer</h1>
+
+            <nav className="mobile-nav">
+              <div
+                className={isActive ? "active ham-menu" : "ham-menu"}
+                onClick={toggleClass}
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </nav>
+
+            <nav
+              className={
+                isActive ? "active off-screen-menu" : "off-screen-menu"
+              }
+              onClick={toggleClass}
+            >
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">Shop</a>
+                </li>
+                <li>
+                  <a href="#">Races</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Profile</a>
+                </li>
+                <li>
+                  <a href="#">Contact Us</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
     </>
   );
 };
